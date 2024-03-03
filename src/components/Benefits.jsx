9cb2794e11f93ea0d5314a03293f9d28d1f51929
emredkyc <emredkyc@hotmail.com>
2024-03-03 21:26:39 +0100

    update title

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -11,7 +11,7 @@ const Benefits = () => {
       <div className="container relative z-2">
         <Heading
           className="md:max-w-md lg:max-w-2xl"
-          title="Chat Smarter, Not Harder with Brainwave"
+          title="Chat Smarter, Not Harder with Brainstorm"
         />
 
         <div className="flex flex-wrap gap-10 mb-10">
