cfc30b09eb5573af29e53091a3be87dbf8c75d9e
emredkyc <emredkyc@hotmail.com>
2024-03-03 09:44:03 +0100

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import "./App.css";
-
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
